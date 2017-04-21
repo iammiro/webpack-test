d6a6762b42ea0077e4dcea91a79fe2e939a3d4d7
iammiro <miroslav.kolomiets@gmail.com>
2017-04-21 11:48:54 +0300

    add sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import TweenMax from 'gsap';
-
 import moment from 'moment';
+import './styles.css';
+import './test.sass';
+
 
 TweenMax.to('h1', 2, {x:100});
 
-console.log((moment().format('MMMM Do YYYY, h:mm:ss a')));
+const date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(date);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, './src'),
@@ -12,6 +13,39 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {presets: ['es2015']},
+                }],
+            },
+            {
+                test: /\.css$/,
+                // use: ['style-loader', 'css-loader']
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader?importLoaders=1&modules=true',
+                }),
+            },
+            {
+                test: /\.sass$/,
+                loader:    ExtractTextPlugin.extract({
+                    loader: 'css-loader!sass-loader',
+                }),
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+            allChunks: true,
+        }),
+    ],
 
     devServer: {
         contentBase: path.resolve(__dirname, './src'),
